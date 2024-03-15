c68e5e78cbfea3facc114986a765036100aabf89
ozkanyllmaz <ozkanyilmaz.dev@gmail.com>
2024-03-15 18:44:24 +0300

    Revert "added"
    This reverts commit 706fa82c5e9b40b1ac74cc5bb01c9cf5129a9193.

--- src/HazineAvi.java
+++
@@ -1,9 +0,0 @@
-public class HazineAvi {
-
-    public static void main(String[] args) {
-        HaraketsizEngeller.duvar();
-        HaraketsizEngeller.kaya();
-        HaraketsizEngeller.dag();
-
-    }
-}
--- src/Uygulama.java
+++
@@ -1,5 +0,0 @@
-// Uygulama içerisinde karakterin hedefe kaç adımda ulaştığı,
-// hangi nesneleri elde ettiği gibi
-// bilgilerin tutulması ve ekranda gösterilmesi sağlamalıdır.
-public class Uygulama {
-}
